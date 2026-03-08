c520a42218443f310de366c2401166cc12eea4e1
MaelSouv <souvestremael@gmail.com>
2026-03-08 21:46:54 +0100

    Correction problème qui empêchait de lancer avec php artisan serve

--- manage_pokemon/resources/views/layouts/app.blade.php
+++ manage_pokemon/resources/views/layouts/app.blade.php
@@ -14,8 +14,10 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    @vite('resources/css/app.css')
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        @vite('resources/css/app.css')
+    @endif
 </head>
 <body>
     <div id="app">
